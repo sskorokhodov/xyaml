6cde85952762e6ecd88a62721d2949d9b8242f7b
Simon Skorokhodov <skorohodovsemen@gmail.com>
2024-02-12 23:08:31 +0100

    [cli] Add a subcommand `exec`, `--input` and `--output` args

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,32 @@ use clap::Arg;
 use clap::ArgAction;
 use clap::Command;
 use serde_yaml::Value;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::io::{self, Read};
+use std::path::PathBuf;
 
 #[macro_export]
 macro_rules! fail {
-    ( $msg: expr ) => {{
-        eprintln!($msg);
+    ( $($msg:expr),* ) => {{
+        eprintln!($($msg),*);
         std::process::exit(1);
-    }};
+    }}
 }
 
-fn main() {
-    let matches = Command::new("xyaml - YAML configuration trasnsformer")
+struct Config {
+    require_null: bool,
+    replacements: Vec<(String, String)>,
+    input: Option<PathBuf>,
+    output: Option<PathBuf>,
+    exec: Option<PathBuf>,
+    exec_args: Vec<String>,
+}
+
+fn config() -> Config {
+    let matches = Command::new("xyaml - YAML configuration transformer")
+        .author("SUPREMATIC Technology Arts GmbH")
         .args([
             Arg::new("require-null")
                 .long("require-null")
@@ -33,55 +47,117 @@ fn main() {
                 .long("input")
                 .value_name("FILE")
                 .help("Read the YAML from <FILE> instead of <stdin>")
+                .value_parser(clap::value_parser!(PathBuf))
                 .num_args(1),
             Arg::new("output")
                 .long("output")
                 .value_name("FILE")
                 .help("Write the result into the <FILE> instead of printing to <stdout>")
-                .num_args(1),
-            Arg::new("exec")
-                .long("exec")
-                .value_name("PATH")
-                .help("Execute the binary at <PATH> after replacing the values")
-                .num_args(1),
-            Arg::new("exec-args")
-                .long("exec-arg")
-                .value_name("ARG")
-                .action(ArgAction::Append)
-                .help("The arguments to the executalbe")
+                .value_parser(clap::value_parser!(PathBuf))
                 .num_args(1),
         ])
+        .subcommand(
+            Command::new("exec").arg(
+                Arg::new("cmd")
+                    .value_name("cmd")
+                    .action(ArgAction::Append)
+                    .help("The executable and its arguments")
+                    .trailing_var_arg(true)
+                    .num_args(1..),
+            ),
+        )
         .get_matches();
 
-    let mut yaml_string = String::new();
-    io::stdin()
-        .read_to_string(&mut yaml_string)
-        .expect("Failed to read from stdin");
+    let env_values = matches.get_flag("env-values");
+    let mut replacements: Vec<_> = matches
+        .get_many::<String>("replacements")
+        .unwrap_or_default()
+        .collect::<Vec<_>>()
+        .chunks(2)
+        .map(|chunk| (chunk[0].clone(), chunk[1].clone()))
+        .collect();
+    if env_values {
+        for entry in replacements.iter_mut() {
+            entry.1 = std::env::var(&entry.1).unwrap_or_else(|e| {
+                fail!(
+                    "Failed to read the referred env variable `{}`\nerror=`{e}`",
+                    entry.1
+                )
+            });
+        }
+    }
+
+    let mut config = Config {
+        require_null: matches.get_flag("require-null"),
+        replacements,
+        output: matches.get_one::<PathBuf>("output").map(Clone::clone),
+        input: matches.get_one::<PathBuf>("input").map(Clone::clone),
+        exec: None,
+        exec_args: vec![],
+    };
+    if let Some(matches) = matches.subcommand_matches("exec") {
+        let cmd: Vec<_> = matches
+            .get_many::<String>("cmd")
+            .unwrap()
+            .map(Clone::clone)
+            .collect();
+        config.exec = Some(PathBuf::from(&cmd[0]));
+        config.exec_args = cmd.into_iter().skip(1).collect();
+    }
+    config
+}
+
+fn main() {
+    let config = config();
+
+    let yaml_string = if let Some(path) = config.input {
+        let mut file = File::open(path.clone())
+            .unwrap_or_else(|e| fail!("Failed to open the intput file `{path:?}`\nerror=`{e}`"));
+        let mut yaml_string = String::new();
+        file.read_to_string(&mut yaml_string)
+            .unwrap_or_else(|e| fail!("Failed to read the intput file `{path:?}`\nerror=`{e}`"));
+        yaml_string
+    } else {
+        let mut yaml_string = String::new();
+        io::stdin()
+            .read_to_string(&mut yaml_string)
+            .expect("Failed to read from stdin");
+        yaml_string
+    };
 
     let mut yaml: Value =
-        serde_yaml::from_str(&yaml_string).unwrap_or_else(|_| fail!("Failed to parse YAML"));
+        serde_yaml::from_str(&yaml_string).unwrap_or_else(|e| fail!("Failed to parse YAML: {e}"));
 
-    let require_null = matches.get_flag("require-null");
-    let env_values = matches.get_flag("env-values");
-    let mut replacements = matches.get_many::<String>("replacements").unwrap();
-    while let Some(path) = replacements.next() {
-        let value = if env_values {
-            let var = replacements.next().expect("no value for path");
-            std::env::var(var)
-                .unwrap_or_else(|_| fail!("the referred env variable `{var}` is not set"))
-        } else {
-            replacements.next().expect("no value for path").clone()
-        };
-        update_value(&mut yaml, path, &value, require_null);
+    for (path, value) in config.replacements.iter() {
+        update_value(&mut yaml, path, value, config.require_null);
     }
 
     let modified_yaml = serde_yaml::to_string(&yaml).expect("Failed to serialize YAML");
-    println!("{}", modified_yaml);
+    if let Some(path) = config.output {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(path.clone())
+            .unwrap_or_else(|e| fail!("Failed to open the output file: {e}"));
+        file.write_all(modified_yaml.as_bytes())
+            .unwrap_or_else(|e| fail!("Failed to write the output file: {e}"));
+    } else {
+        println!("{}", modified_yaml);
+    }
+
+    if let Some(path) = config.exec {
+        let mut cmd = std::process::Command::new(path);
+        let cmd = cmd.args(&config.exec_args);
+        let mut handle = cmd
+            .spawn()
+            .unwrap_or_else(|e| fail!("Failed to spawn the process:\ncmd=`{cmd:?}`\nerror=`{e}`"));
+        handle.wait().ok();
+    }
 }
 
 fn update_value(obj: &mut Value, path: &str, new_value: &str, require_null: bool) {
     let segments: Value = serde_yaml::from_str(path)
-        .unwrap_or_else(|_| fail!("Failed to parse the path as YAML:\n`{path}`"));
+        .unwrap_or_else(|e| fail!("Failed to parse the path as YAML:\n`{path}`\nerror: {e}"));
     if !segments.is_sequence() {
         fail!("Path is not a YAML sequence:\n`{path}`")
     }
@@ -94,6 +170,7 @@ fn update_value(obj: &mut Value, path: &str, new_value: &str, require_null: bool
             .trim_end_matches('\n')
             .to_string();
         cursor.push(segment_str.clone());
+
         if segment.is_sequence() {
             let seq = segment.as_sequence().unwrap();
             if seq.len() != 1 {
@@ -116,7 +193,7 @@ fn update_value(obj: &mut Value, path: &str, new_value: &str, require_null: bool
     if require_null && !current_obj.is_null() {
         fail!("Object at path is not `null`:\n  obj={current_obj:?}\n  path=`{path}`");
     }
-    *current_obj = serde_yaml::from_str(new_value).unwrap_or_else(|_| {
-        fail!("New value is no a valid YAML:\n  new_value=`{new_value}`\n  path=`{path}`")
+    *current_obj = serde_yaml::from_str(new_value).unwrap_or_else(|e| {
+        fail!("New value is no a valid YAML:\n  new_value=`{new_value}`\n  path=`{path}`\n  error=`{e}`")
     });
 }
